c76f08bf39cbc9c9c2d8cc22038f35bbe93f52f2
Katongole Allan <kapsonkatongole@gmail.com>
2019-07-14 23:58:31 +0300

    Add structures models

--- le_me_project/structures/models.py
+++ le_me_project/structures/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from .choices import REGIONS
 
-# Create your models here.
+class District(models.Model):
+    district_name           = models.CharField(max_length=100)
+    district_region         = models.CharField(max_length=20, choices=REGIONS)
+    district_size_hectares  = models.IntegerField()
+    date_created            = models.DateTimeField(auto_now_add=True)
